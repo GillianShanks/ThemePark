cc9eae103ca85f27a49983feb53763f82eef454e
Gillian Shanks <GillianShanks88@gmail.com>
2019-10-01 16:58:02 +0100

    sets up interfaces and applies ISecurity to rollercoaster - tests for pass and fail - with both conditions not met and with only one condition met.

--- src/test/java/attractions/RollercoasterTest.java
+++ src/test/java/attractions/RollercoasterTest.java
@@ -2,16 +2,23 @@ package attractions;
 
 import org.junit.Before;
 import org.junit.Test;
+import people.Visitor;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class RollercoasterTest {
 
     RollerCoaster rollerCoaster;
+    Visitor youngVisitor;
+    Visitor oldVisitor;
+    Visitor youngTallVisitor;
 
     @Before
     public void setUp() {
         rollerCoaster = new RollerCoaster("Blue Ridge", 10);
+        youngVisitor = new Visitor(7, 100, 10);
+        youngTallVisitor = new Visitor (11, 150, 10);
+        oldVisitor = new Visitor(20, 180, 30);
     }
 
     @Test
@@ -27,5 +34,20 @@ public class RollercoasterTest {
     @Test
     public void hasVisitCount() {
         assertEquals(0, rollerCoaster.getVisitCount());
+    }
+
+    @Test
+    public void checksAllowedToRide__oldAndTall() {
+        assertTrue(rollerCoaster.isAllowedTo(oldVisitor));
+    }
+
+    @Test
+    public void checksAllowedToRide__youngAndTall() {
+        assertFalse(rollerCoaster.isAllowedTo(youngTallVisitor));
+    }
+
+    @Test
+    public void checksAllowedToRide__youngAndshort() {
+        assertFalse(rollerCoaster.isAllowedTo(youngVisitor));
     }
 }
