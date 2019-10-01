e7b8996fc147f2bfc3f08136d487dba93ef41476
Gillian Shanks <GillianShanks88@gmail.com>
2019-10-01 16:41:08 +0100

    sets up interfaces and applies ISecurity to playground - tests for pass and fail.

--- src/test/java/attractions/PlaygroundTest.java
+++ src/test/java/attractions/PlaygroundTest.java
@@ -2,15 +2,20 @@ package attractions;
 
 import org.junit.Before;
 import org.junit.Test;
+import people.Visitor;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class PlaygroundTest {
     Playground playground;
+    Visitor youngVisitor;
+    Visitor oldVisitor;
 
     @Before
     public void setUp() throws Exception {
         playground = new Playground("Fun Zone", 7);
+        youngVisitor = new Visitor(7, 100, 10);
+        oldVisitor = new Visitor(20, 180, 30);
     }
 
     @Test
@@ -26,5 +31,15 @@ public class PlaygroundTest {
     @Test
     public void hasVisitCount() {
         assertEquals(0, playground.getVisitCount());
+    }
+
+    @Test
+    public void checksAllowedToEnter__pass() {
+        assertTrue(playground.isAllowedTo(youngVisitor));
+    }
+
+    @Test
+    public void checksAllowedToEnter__fail() {
+        assertFalse(playground.isAllowedTo(oldVisitor));
     }
 }
